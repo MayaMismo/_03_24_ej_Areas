8c3cbbc191f702d9ee951ef9257c9fd374c08fc2
MayaMismo <mjrodriher@gmail.com>
2023-03-24 12:12:23 +0100

    version 12:12

--- src/com/adecco/modelo/Area.java
+++ src/com/adecco/modelo/Area.java
@@ -1,2 +1,16 @@
-package com.adecco.modelo;public class Area {
+package com.adecco.modelo;
+//Desde la clase Areas crear métodos para calcular el área de un círculo y un rectángulo y mostrarlos en pantalla
+public class Area {
+
+//    public String areaCirculo(float radio){
+//        return "El área del círculo es " + 2*3.1416*radio;
+//    }
+    //si quiero poner el texto en el resultado, lo tengo que poner como tipo float y poner
+    //la f en los números, en este caso en pi 3.1416f
+    public float areaCirculo(float radio){
+        return  3.1416f*radio*radio;
+    }
+    public String areaRectangulo (float lado, float altura){
+        return "El área del rectángulo es: " + lado * altura;
+    }
 }
--- src/com/adecco/presentacion/Main.java
+++ src/com/adecco/presentacion/Main.java
@@ -1,5 +1,18 @@
+package com.adecco.presentacion;
+
+import com.adecco.modelo.Area;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //tenemos que generar un objeto para poder imprimir el resultado
+        Area a1 = new Area();
+
+        //Area a2 = new Area();
+
+        //y aquí los mando llamar. No seria necesario crear dos objetos para llamar el resultado,
+        //desperdiciamos espacio
+        System.out.println("El área del círculo es: " +a1.areaCirculo(2f));
+        System.out.println(a1.areaRectangulo(5f,6f));
+
     }
 }
